c16b4bc55915357c0cc2067c3e69221fe7f3d53c
mustkov <dolzhenkov74@gmail.com>
2022-05-08 15:50:16 +0400

    feat: add main functional

--- src/script.js
+++ src/script.js
@@ -286,3 +286,147 @@ const languageChange = () => {
   }
   shiftButton = false;
 };
+
+row.forEach((e) => {
+  e.addEventListener("click", (event) => {
+    input.focus();
+
+    if (event.target.textContent.length === 1) {
+      if (input.selectionStart === input.value.length) {
+        cursor = input.value.length;
+      }
+      if (input.selectionStart !== input.selectionEnd) {
+        let text = [...input.value];
+        const currentTarget = event.target.textContent;
+        text.splice(
+          input.selectionStart,
+          input.selectionEnd - input.selectionStart,
+          currentTarget
+        );
+        text = text.join("");
+        input.value = text;
+        cursor += 1;
+        input.setSelectionRange(cursor, cursor);
+      } else {
+        input.setSelectionRange(cursor, cursor);
+        const text = [...input.value];
+        text.splice(cursor, 0, event.target.textContent);
+        input.value = text.join("");
+        cursor += 1;
+        input.setSelectionRange(cursor, cursor);
+      }
+      if (shiftButton === true) {
+        shiftUp();
+        shiftButton = false;
+      }
+    } else if (event.target.textContent === "CapsLock") {
+      capsChange();
+    } else if (event.target.textContent === "Tab") {
+      tabChange();
+    } else if (event.target.textContent === "Backspace") {
+      backspaceChange();
+    } else if (event.target.textContent === "Del") {
+      deleteChange();
+    } else if (event.target.textContent === "Enter") {
+      enterChange();
+    } else if (
+      event.target.textContent === "en" ||
+      event.target.textContent === "ru"
+    ) {
+      languageChange();
+    } else if (event.target.textContent === "Shift") {
+      if (shiftButton === false) {
+        shiftDown();
+        shiftButton = true;
+      } else {
+        shiftUp();
+        shiftButton = false;
+      }
+    }
+  });
+});
+
+document.addEventListener("keydown", (e) => {
+  document.querySelector(`.${e.code}`).classList.add("key-active");
+  if (e.altKey && e.shiftKey) {
+    languageChange();
+  }
+  if (e.key === "Alt" || e.key === "AltGraph") {
+    e.preventDefault();
+  }
+  if (e.key === "Shift") {
+    e.preventDefault();
+    shiftChange(e);
+  } else if (e.key === "CapsLock") {
+    e.preventDefault();
+    capsChange();
+  } else if (e.key === "Tab") {
+    e.preventDefault();
+    tabChange();
+  } else if (e.key === "Backspace") {
+    e.preventDefault();
+    backspaceChange();
+  } else if (e.key === "Delete") {
+    e.preventDefault();
+    deleteChange();
+  } else if (e.key === "Enter") {
+    e.preventDefault();
+    enterChange();
+  } else if (e.key.length === 1) {
+    e.preventDefault();
+    input.focus();
+    if (input.selectionStart !== input.selectionEnd) {
+      let text = [...input.value];
+      const cur = document.querySelector(`.${e.code}`).textContent;
+      text.splice(
+        input.selectionStart,
+        input.selectionEnd - input.selectionStart,
+        cur
+      );
+      text = text.join("");
+      input.value = text;
+      cursor += 1;
+      input.setSelectionRange(cursor, cursor);
+    } else {
+      input.setSelectionRange(cursor, cursor);
+      let text = [...input.value];
+      const cur = document.querySelector(`.${e.code}`).textContent;
+      text.splice(cursor, 0, cur);
+      text = text.join("");
+      input.value = text;
+      cursor += 1;
+      input.setSelectionRange(cursor, cursor);
+    }
+    if (shiftButton === true) {
+      shiftUp();
+      shiftButton = false;
+    }
+  }
+});
+
+document.addEventListener("keyup", (e) => {
+  document.querySelector(`.${e.code}`).classList.remove("key-active");
+  if (e.key === "Shift") {
+    shiftChange(e);
+  } else if (e.key.includes("Arrow")) {
+    input.focus();
+    cursor = input.selectionStart;
+    input.setSelectionRange(cursor, cursor);
+  }
+});
+
+input.addEventListener("click", () => {
+  if (input.selectionStart !== input.selectionEnd) {
+    input.setSelectionRange(input.selectionStart, input.selectionEnd);
+    cursor = input.selectionStart;
+  } else {
+    cursor = input.selectionStart;
+    input.setSelectionRange(cursor, cursor);
+  }
+});
+
+const setLocalStorage = () => {
+  localStorage.setItem("language", language);
+  localStorage.setItem("checkCaps", checkCaps);
+};
+window.addEventListener("beforeunload", setLocalStorage);
